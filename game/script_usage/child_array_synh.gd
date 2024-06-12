3dd0735a9f555387640f81d48640ec63c3b4dcf3
JimmyLoar <play1596357895@gmail.com>
2024-06-12 08:29:00 +0300

    Update child_array_synh.gd

--- game/script_usage/child_array_synh.gd
+++ game/script_usage/child_array_synh.gd
@@ -1,3 +1,4 @@
+@tool
 class_name ChildArraySynhronizer
 
 var parent: Node
@@ -14,8 +15,15 @@ func _init(_parent: Node, _prototype: Node):
 	logger.debug("created new synhronizer | parent %s | prototype %s" % [_parent, _prototype])
 
 
-func synh_children(list: Array):
-	var difference: int = parent.get_child_count() - list.size()
+func synh_array(list: Array):
+	_synh(parent.get_child_count() - list.size())
+
+
+func synh_count(count: int):
+	_synh(parent.get_child_count() - count)
+
+
+func _synh(difference: int):
 	if difference > 0: _add_childs(difference)
 	elif difference < 0: _remove_childs(abs(difference))
 
