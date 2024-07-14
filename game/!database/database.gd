080ff5a8fdcf4d96b738e66ed55ff9593b9a39ed
JimmyLoar <play1596357895@gmail.com>
2024-07-14 03:56:38 +0300

    feat: database added a method to retrieve lib keys

--- game/!database/database.gd
+++ game/!database/database.gd
@@ -15,20 +15,29 @@ var _data := {
 	level = DataLib.new({
 		
 	}),
-	upgrade = DataLib.new({
-		&"damage_1": preload('res://resources/updrades/gun/damage_1.tres'), 
-		&"firerate_1": preload('res://resources/updrades/gun/firerate_1.tres'), 
-		&"range_1": preload('res://resources/updrades/gun/range_1.tres'),
+	upgrade_tree = DataLib.new({
+		&"gun_t1_v1": preload('res://resources/upgrade_tree/gun_t1_v1.tscn'),
 	}),
 }
 
 
 @onready var _logger := GodotLogger.with("Database")
 
-func get_enemies_lib() -> DataLib: return _data.enemy
+func get_enemy_lib() -> DataLib: return _data.enemy
+func get_enemy_keys() -> Array: 
+	return get_enemy_lib().get_all_data().keys()
+
 func get_towers_lib()  -> DataLib: return _data.tower
+func get_towers_keys() -> Array: 
+	return get_towers_lib().get_all_data().keys()
+
 func get_level_lib()   -> DataLib: return _data.level
-func get_upgrade_lib() -> DataLib: return _data.upgrade
+func get_level_keys() -> Array: 
+	return get_level_lib().get_all_data().keys()
+
+func get_upgrade_lib() -> DataLib: return _data.upgrade_tree
+func get_upgrade_keys() -> Array: 
+	return get_upgrade_lib().get_all_data().keys()
 
 
 func _ready() -> void:
