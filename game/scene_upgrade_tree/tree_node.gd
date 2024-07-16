9ec8f9bd3f8d56d63d8372e6b3ef7d33bbf20947
JimmyLoar <play1596357895@gmail.com>
2024-07-16 14:48:36 +0300

    Update tree_node.gd

--- game/scene_upgrade_tree/tree_node.gd
+++ game/scene_upgrade_tree/tree_node.gd
@@ -2,6 +2,10 @@
 class_name TreeNode
 extends Marker2D
 
+signal unloked(node: TreeNode)
+
+
+
 @export var skill_name := "Name"
 @export var icon: Texture = preload('res://icon.svg'):
 	set(value):
@@ -12,9 +16,9 @@ extends Marker2D
 
 @export var dependence: Array[TreeNode] = []
 @export var prise = {
-	"1": 0,
-	"2": 0,
-	"3": 0,
+	"money":  0,
+	"energy": 0,
+	"scrap":  0,
 }
 
 
@@ -23,6 +27,9 @@ extends Marker2D
 @export var _ui_right: TreeNode
 @export var _ui_top: TreeNode
 @export var _ui_bottom: TreeNode
+
+
+var _unlocked := false
 
 
 @onready var ui: PanelContainer = $UI
@@ -36,22 +43,17 @@ func _ready() -> void:
 		ui.set_title(self.name)
 	
 	else:
-		ui.set_title("Node")
 		call_deferred("_update_focus_button")
+		update()
+		_connect_to_deperance()
 
 
-func _update_focus_button():
-	if _ui_left:
-		button.focus_neighbor_left = _ui_left.button.get_path()
-	if _ui_right:
-		button.focus_neighbor_right = _ui_right.button.get_path()
-	if _ui_top:
-		button.focus_neighbor_top = _ui_top.button.get_path()
-	if _ui_bottom:
-		button.focus_neighbor_bottom = _ui_bottom.button.get_path()
+func update():
+	ui.set_title(skill_name)
+	modulate = Color.WHITE if is_dependence_unlocked() else Color(1, 1, 1, 0.5)
 
 
-func get_connect_points() -> Array[Node]:
+func get_connect_markers() -> Array[Node]:
 	return connection_markers.get_children()
 
 
@@ -66,3 +68,32 @@ func get_down_marker() -> Marker2D:
 
 func get_up_marker() -> Marker2D:
 	return connection_markers.up
+
+
+func is_unlock() -> bool: return _unlocked
+
+
+func is_dependence_unlocked() -> bool:
+	if dependence.is_empty(): return true
+	return dependence.all(
+		func(node: TreeNode):
+			return node.is_unlock()
+	)
+
+
+func _connect_to_deperance():
+	for node: TreeNode in dependence:
+		node.unloked.connect(update)
+
+
+func _update_focus_button():
+	if _ui_left:
+		button.focus_neighbor_left = _ui_left.button.get_path()
+	if _ui_right:
+		button.focus_neighbor_right = _ui_right.button.get_path()
+	if _ui_top:
+		button.focus_neighbor_top = _ui_top.button.get_path()
+	if _ui_bottom:
+		button.focus_neighbor_bottom = _ui_bottom.button.get_path()
+
+
